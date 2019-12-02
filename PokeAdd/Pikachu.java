8776bcb65ad2504d18ae79792bcd9b79876c94f9
Justtom25 <35940675+Justtom25@users.noreply.github.com>
2019-12-01 21:47:20 -0500

    PokeAdd is finished

---
+++ PokeAdd/Pikachu.java
@@ -0,0 +1,57 @@
+public class Pikachu extends Pokemon{
+		name = "Pikachu";
+		bHP = 4;
+		bAtk = 6;
+		bDef = 4;
+		bSpAtk = 5;
+		bSpDef = 5;
+		bSpd = 9;
+		type1 = "Electric";
+		type2 = "";
+		abilities = new String[]{"Static", "Cute Charm", "Lightning Rod", "Sprint", "Sequence"};
+		abilityTypeCount = new int[] {2,2,1};
+		previous = new Pichu();
+		prevReq = 10;
+		next = new Raichu();
+		nextReq = 20;
+		height = "1\'/4\"";
+		size = "Small";
+		weight = 13.2;
+		WC = 1;
+		maleRatio = 50;
+		femaleRatio = 50;
+		egg1 = "Field";
+		egg2 = "Fairy";
+		habitats = new String[]{"Forest", "Grassland", "Urban"};
+		overland = 7;
+		swim = 2;
+		levitate = 0;
+		sky = 0;
+		burrow = 0;
+		lJump = 2;
+		hJump = 2;
+		power = 2;
+		natureWalk = new String[]{"Forest", "Urban"};
+		underdog = true;
+		other = new String[]{"Zapper"};
+		athl = 3;
+		acro = 3;
+		combat = 2;
+		stealth = 3;
+		percep = 2;
+		focus = 3;
+		athlBouns = 0;
+		acroBonus = 1;
+		combatBouns = 0;
+		stealthBonus = 1;
+		percepBouns = 1;
+		focusBonus = 2;
+		movesLevels = new int[]{5, 7, 10, 13, 18, 21, 23, 26, 29, 34, 37, 42, 45, 50, 53, 58};
+		moves = new String[]{"Growl", "Play Nice", "Quick Attack", "Electro Ball", "Thunder Wave", "Feint", "Double Team", "Spark", "Nuzzle", "Discharge", "Slam", "Thunderbolt", "Agility", "Wild Charge", "Light Screen", "Thunder"};
+		TMs = new String[]{"Toxic", "Hidden Power", "Light Screen", "Protect", "Rain Dance", "Frustration", "Thunderbolt", "Thunder", "Return", "Brick Break", "Double Team", "Facade", "Rest", "Attract", "Round", "Echoed Voice", "Fling", "Charge Beam", "Volt Switch", "Thunder Wave", "Grass Knot", "Swagger", "Sleep Talk", "Substitute", "Wild Charge", "Confide"};
+		tutorMoves = new String[]{"Body Slam", "Slam", "Counter", "Covet", "Defense Curl", "Dynamic Punch", "Electroweb", "Focus Punch", "Helping Hand", "Hold Hands", "Iron Tail", "Knock Off", "Laser Focus", "Magnet Rise", "Mega Kick", "Mega Punch", "Rollout", "Seismic Toss", "Shock Wave", "Signal Beam", "Snore", "Swift", "Thunder Punch", "Uproar", "Volt Tackle", "Tackle"};
+		Pikachu(){
+
+		super();
+		}
+}
---
+++ PokeAdd/a.java
@@ -0,0 +1,17 @@
+public class a extends Pokemon{
+	a(){
+		name = "a";
+		bHP = 10;
+		bAtk = 7;
+		bDef = 8;
+		bSpAtk = 7;
+		bSpDef = 9;
+		bbSpd = 5;
+		type1 = "Grass";
+		type2 = "Flying";
+		abilities1 = "Chlorophyll";
+		abilities2 = "Solar Power";
+		abilities3 = "Photosynthesis";
+		abilities4 = "Harvest";
+		abilities5 = "Windveiled";
+}
