5a0091139d5b023558034761831acde499a91b50
Blake Morgan <blake.morgan.19@cnu.edu>
2020-01-23 19:37:24 -0500

    Added some things

--- src/EnviroDriver.java
+++ src/EnviroDriver.java
@@ -1,19 +1,79 @@
 
 /**
- *  Generates a Pokemon based on given environment, min level, and max level
+ * Generates a Pokemon based on given environment, min level, and max level
  * 
- *  @author Mageman460/GrandMage460
- *  @version 
+ * @author Mageman460/GrandMage460
+ * @version
  */
 public class EnviroDriver {
-    
-    
-    
+
+    private final static String[] ARCTIC   = { "" };
+    private final static String[] BEACH    = { "" };
+    private final static String[] CAVE     = { "" };
+    private final static String[] DESERT   = { "" };
+    private final static String[] FOREST   = { "" };
+    private final static String[] FRESH    = { "" };
+    private final static String[] GRASS    = { "" };
+    private final static String[] MARSH    = { "" };
+    private final static String[] MOUNTAIN = { "" };
+    private final static String[] OCEAN    = { "" };
+    private final static String[] RAIN     = { "" };
+    private final static String[] TAIGA    = { "" };
+    private final static String[] TUNDRAA  = { "" };
+    private final static String[] URBAN    = { "" };
+
+
     public static void main(String[] args) {
         String enviro = args[0];
         int minLevel = Integer.valueOf(args[1]);
         int maxLevel = Integer.valueOf(args[2]);
-        
+        String name = "";
+        switch (enviro) {
+            case "Arctic":
+                name = ARCTIC[((int)Math.random() * ARCTIC.length)];
+                break;
+            case "Beach":
+                name = BEACH[((int)Math.random() * BEACH.length)];
+                break;
+            case "Cave":
+                name = CAVE[((int)Math.random() * CAVE.length)];
+                break;
+            case "Desert":
+                name = DESERT[((int)Math.random() * DESERT.length)];
+                break;
+            case "Forest":
+                name = FOREST[((int)Math.random() * FOREST.length)];
+                break;
+            case "Fresh Water":
+                name = FRESH[((int)Math.random() * FRESH.length)];
+                break;
+            case "Grasslands":
+                name = GRASS[((int)Math.random() * GRASS.length)];
+                break;
+            case "Marsh":
+                name = MARSH[((int)Math.random() * MARSH.length)];
+                break;
+            case "Mountain":
+                name = MOUNTAIN[((int)Math.random() * MOUNTAIN.length)];
+                break;
+            case "Rainforest":
+                name = RAIN[((int)Math.random() * RAIN.length)];
+                break;
+            case "Ocean":
+                name = OCEAN[((int)Math.random() * OCEAN.length)];
+                break;
+            case "Taiga":
+                name = TAIGA[((int)Math.random() * TAIGA.length)];
+                break;
+            case "Tundraa":
+                name = TUNDRAA[((int)Math.random() * TUNDRAA.length)];
+                break;
+            case "Urban":
+                name = URBAN[((int)Math.random() * URBAN.length)];
+                break;
+        }
+        Generator genned = new Generator(name, minLevel, maxLevel);
+        System.out.println(genned.toJSON());
     }
 
 }
--- src/Generator.java
+++ src/Generator.java
@@ -1,66 +1,70 @@
 import java.lang.Class;
+
 /**
- * Creates the actual Pokemon that is used
- * 
- * Author : MageMan460/GrandMage460
- * 
+ * Creates the actual Pokemon that is used Author : MageMan460/GrandMage460
  */
 public class Generator {
-    //private String[] pokeList = {"Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran♀", "Nidorina", "Nidoqueen", "Nidoran♂", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch’d", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew", "Chikorita", "Bayleef", "Meganium", "Cyndaquil", "Quilava", "Typhlosion", "Totodile", "Croconaw", "Feraligatr", "Sentret", "Furret", "Hoothoot", "Noctowl", "Ledyba", "Ledian", "Spinarak", "Ariados", "Crobat", "Chinchou", "Lanturn", "Pichu", "Cleffa", "Igglybuff", "Togepi", "Togetic", "Natu", "Xatu", "Mareep", "Flaaffy", "Ampharos", "Bellossom", "Marill", "Azumarill", "Sudowoodo", "Politoed", "Hoppip", "Skiploom", "Jumpluff", "Aipom", "Sunkern", "Sunflora", "Yanma", "Wooper", "Quagsire", "Espeon", "Umbreon", "Murkrow", "Slowking", "Misdreavus", "Unown", "Wobbuffet", "Girafarig", "Pineco", "Forretress", "Dunsparce", "Gligar", "Steelix", "Snubbull", "Granbull", "Qwilfish", "Scizor", "Shuckle", "Heracross", "Sneasel", "Teddiursa", "Ursaring", "Slugma", "Magcargo", "Swinub", "Piloswine", "Corsola", "Remoraid", "Octillery", "Delibird", "Mantine", "Skarmory", "Houndour", "Houndoom", "Kingdra", "Phanpy", "Donphan", "Porygon2", "Stantler", "Smeargle", "Tyrogue", "Hitmontop", "Smoochum", "Elekid", "Magby", "Miltank", "Blissey", "Raikou", "Entei", "Suicune", "Larvitar", "Pupitar", "Tyranitar", "Lugia", "Ho-Oh", "Celebi", "Treecko", "Grovyle", "Sceptile", "Torchic", "Combusken", "Blaziken", "Mudkip", "Marshtomp", "Swampert", "Poochyena", "Mightyena", "Zigzagoon", "Linoone", "Wurmple", "Silcoon", "Beautifly", "Cascoon", "Dustox", "Lotad", "Lombre", "Ludicolo", "Seedot", "Nuzleaf", "Shiftry", "Taillow", "Swellow", "Wingull", "Pelipper", "Ralts", "Kirlia", "Gardevoir", "Surskit", "Masquerain", "Shroomish", "Breloom", "Slakoth", "Vigoroth", "Slaking", "Nincada", "Ninjask", "Shedinja", "Whismur", "Loudred", "Exploud", "Makuhita", "Hariyama", "Azurill", "Nosepass", "Skitty", "Delcatty", "Sableye", "Mawile", "Aron", "Lairon", "Aggron", "Meditite", "Medicham", "Electrike", "Manectric", "Plusle", "Minun", "Volbeat", "Illumise", "Roselia", "Gulpin", "Swalot", "Carvanha", "Sharpedo", "Wailmer", "Wailord", "Numel", "Camerupt", "Torkoal", "Spoink", "Grumpig", "Spinda", "Trapinch", "Vibrava", "Flygon", "Cacnea", "Cacturne", "Swablu", "Altaria", "Zangoose", "Seviper", "Lunatone", "Solrock", "Barboach", "Whiscash", "Corphish", "Crawdaunt", "Baltoy", "Claydol", "Lileep", "Cradily", "Anorith", "Armaldo", "Feebas", "Milotic", "Castform", "Kecleon", "Shuppet", "Banette", "Duskull", "Dusclops", "Tropius", "Chimecho", "Absol", "Wynaut", "Snorunt", "Glalie", "Spheal", "Sealeo", "Walrein", "Clamperl", "Huntail", "Gorebyss", "Relicanth", "Luvdisc", "Bagon", "Shelgon", "Salamence", "Beldum", "Metang", "Metagross", "Regirock", "Regice", "Registeel", "Latias", "Latios", "Kyogre", "Groudon", "Rayquaza", "Jirachi", "Deoxys", "Turtwig", "Grotle", "Torterra", "Chimchar", "Monferno", "Infernape", "Piplup", "Prinplup", "Empoleon", "Starly", "Staravia", "Staraptor", "Bidoof", "Bibarel", "Kricketot", "Kricketune", "Shinx", "Luxio", "Luxray", "Budew", "Roserade", "Cranidos", "Rampardos", "Shieldon", "Bastiodon", "Burmy", "Wormadam", "Mothim", "Combee", "Vespiquen", "Pachirisu", "Buizel", "Floatzel", "Cherubi", "Cherrim", "Shellos", "Gastrodon", "Ambipom", "Drifloon", "Drifblim", "Buneary", "Lopunny", "Mismagius", "Honchkrow", "Glameow", "Purugly", "Chingling", "Stunky", "Skuntank", "Bronzor", "Bronzong", "Bonsly", "Mime Jr.", "Happiny", "Chatot", "Spiritomb", "Gible", "Gabite", "Garchomp", "Munchlax", "Riolu", "Lucario", "Hippopotas", "Hippowdon", "Skorupi", "Drapion", "Croagunk", "Toxicroak", "Carnivine", "Finneon", "Lumineon", "Mantyke", "Snover", "Abomasnow", "Weavile", "Magnezone", "Lickilicky", "Rhyperior", "Tangrowth", "Electivire", "Magmortar", "Togekiss", "Yanmega", "Leafeon", "Glaceon", "Gliscor", "Mamoswine", "Porygon-Z", "Gallade", "Probopass", "Dusknoir", "Froslass", "Rotom", "Uxie", "Mesprit", "Azelf", "Dialga", "Palkia", "Heatran", "Regigigas", "Giratina", "Cresselia", "Phione", "Manaphy", "Darkrai", "Shaymin", "Arceus", "Victini", "Snivy", "Servine", "Serperior", "Tepig", "Pignite", "Emboar", "Oshawott", "Dewott", "Samurott", "Patrat", "Watchog", "Lillipup", "Herdier", "Stoutland", "Purrloin", "Liepard", "Pansage", "Simisage", "Pansear", "Simisear", "Panpour", "Simipour", "Munna", "Musharna", "Pidove", "Tranquill", "Unfezant", "Blitzle", "Zebstrika", "Roggenrola", "Boldore", "Gigalith", "Woobat", "Swoobat", "Drilbur", "Excadrill", "Audino", "Timburr", "Gurdurr", "Conkeldurr", "Tympole", "Palpitoad", "Seismitoad", "Throh", "Sawk", "Sewaddle", "Swadloon", "Leavanny", "Venipede", "Whirlipede", "Scolipede", "Cottonee", "Whimsicott", "Petilil", "Lilligant", "Basculin", "Sandile", "Krokorok", "Krookodile", "Darumaka", "Darmanitan", "Maractus", "Dwebble", "Crustle", "Scraggy", "Scrafty", "Sigilyph", "Yamask", "Cofagrigus", "Tirtouga", "Carracosta", "Archen", "Archeops", "Trubbish", "Garbodor", "Zorua", "Zoroark", "Minccino", "Cinccino", "Gothita", "Gothorita", "Gothitelle", "Solosis", "Duosion", "Reuniclus", "Ducklett", "Swanna", "Vanillite", "Vanillish", "Vanilluxe", "Deerling", "Sawsbuck", "Emolga", "Karrablast", "Escavalier", "Foongus", "Amoonguss", "Frillish", "Jellicent", "Alomomola", "Joltik", "Galvantula", "Ferroseed", "Ferrothorn", "Klink", "Klang", "Klinklang", "Tynamo", "Eelektrik", "Eelektross", "Elgyem", "Beheeyem", "Litwick", "Lampent", "Chandelure", "Axew", "Fraxure", "Haxorus", "Cubchoo", "Beartic", "Cryogonal", "Shelmet", "Accelgor", "Stunfisk", "Mienfoo", "Mienshao", "Druddigon", "Golett", "Golurk", "Pawniard", "Bisharp", "Bouffalant", "Rufflet", "Braviary", "Vullaby", "Mandibuzz", "Heatmor", "Durant", "Deino", "Zweilous", "Hydreigon", "Larvesta", "Volcarona", "Cobalion", "Terrakion", "Virizion", "Tornadus", "Thundurus", "Reshiram", "Zekrom", "Landorus", "Kyurem", "Keldeo", "Meloetta", "Genesect", "Chespin", "Quilladin", "Chesnaught", "Fennekin", "Braixen", "Delphox", "Froakie", "Frogadier", "Greninja", "Bunnelby", "Diggersby", "Fletchling", "Fletchinder", "Talonflame", "Scatterbug", "Spewpa", "Vivillon", "Litleo", "Pyroar", "Flabébé", "Floette", "Florges", "Skiddo", "Gogoat", "Pancham", "Pangoro", "Furfrou", "Espurr", "Meowstic", "Honedge", "Doublade", "Aegislash", "Spritzee", "Aromatisse", "Swirlix", "Slurpuff", "Inkay", "Malamar", "Binacle", "Barbaracle", "Skrelp", "Dragalge", "Clauncher", "Clawitzer", "Helioptile", "Heliolisk", "Tyrunt", "Tyrantrum", "Amaura", "Aurorus", "Sylveon", "Hawlucha", "Dedenne", "Carbink", "Goomy", "Sliggoo", "Goodra", "Klefki", "Phantump", "Trevenant", "Pumpkaboo", "Gourgeist", "Bergmite", "Avalugg", "Noibat", "Noivern", "Xerneas", "Yveltal", "Zygarde", "Diancie", "Hoopa", "Volcanion", "Rowlet", "Dartrix", "Decidueye", "Litten", "Torracat", "Incineroar", "Popplio", "Brionne", "Primarina", "Pikipek", "Trumbeak", "Toucannon", "Yungoos", "Gumshoos", "Grubbin", "Charjabug", "Vikavolt", "Crabrawler", "Crabominable", "Oricorio", "Cutiefly", "Ribombee", "Rockruff", "Lycanroc", "Wishiwashi", "Mareanie", "Toxapex", "Mudbray", "Mudsdale", "Dewpider", "Araquanid", "Fomantis", "Lurantis", "Morelull", "Shiinotic", "Salandit", "Salazzle", "Stufful", "Bewear", "Bounsweet", "Steenee", "Tsareena", "Comfey", "Oranguru", "Passimian", "Wimpod", "Golisopod", "Sandygast", "Palossand", "Pyukumuku", "Type: Null", "Silvally", "Minior", "Komala", "Turtonator", "Togedemaru", "Mimikyu", "Bruxish", "Drampa", "Dhelmise", "Jangmo-o", "Hakamo-o", "Kommo-o", "Tapu Koko", "Tapu Lele", "Tapu Bulu", "Tapu Fini", "Cosmog", "Cosmoem", "Solgaleo", "Lunala", "Nihilego", "Buzzwole", "Pheromosa", "Xurkitree", "Celesteela", "Kartana", "Guzzlord", "Necrozma", "Magearna", "Marshadow", "Poipole", "Naganadel", "Stakataka", "Blacephalon", "Zeraora", "Meltan", "Melmetal"};
+
     @SuppressWarnings("unused")
     private String[] moves;
     @SuppressWarnings("unused")
     private String[] abilities;
-    private Pokemon genned;
-    
+    private Pokemon  genned;
+
     public Generator(String name, int minLevel, int maxLevel) {
-        if(minLevel > maxLevel) {
+        if (minLevel > maxLevel) {
             int temp = minLevel;
             minLevel = maxLevel;
             maxLevel = temp;
         }
         try {
             @SuppressWarnings("unchecked")
-            Class<Pokemon> p1 = (Class<Pokemon>) Class.forName(name);
-            genned = (Pokemon) p1.newInstance();
+            Class<Pokemon> p1 = (Class<Pokemon>)Class.forName(name);
+            genned = (Pokemon)p1.newInstance();
             try {
-               genned.setNature();
-               int levels = (int)(Math.random() * maxLevel - minLevel + 1) + minLevel;
-               genned.normalize();
-               genned.levelUp(levels);
-               this.moves = genned.generateMoveList();
-               this.abilities = genned.generateAbilities();
-               genned.update();
-            }catch(Exception e) {
-    
+                genned.setNature();
+                int levels =
+                    (int)(Math.random() * maxLevel - minLevel + 1) + minLevel;
+                genned.normalize();
+                genned.levelUp(levels);
+                this.moves = genned.generateMoveList();
+                this.abilities = genned.generateAbilities();
+                genned.update();
             }
-        }catch(ClassNotFoundException e) {
+            catch (Exception e) {
+
+            }
+        }
+        catch (ClassNotFoundException e) {
             System.out.println("Not a Pokemon name");
-        }catch(IllegalAccessException e) {
-            
-        }catch (InstantiationException e1) {
-            
+        }
+        catch (IllegalAccessException e) {
+
+        }
+        catch (InstantiationException e1) {
+
         }
     }
-    
+
+
     public String toJSON() {
         return genned.toJSON();
     }
-    
-	public static void main(String[] args) {
-	    Pokemon t = new Pikachu();
-	    t.setNature();
-	    t.normalize();
-	    t.levelUp(10);
-	    String[] moves = t.generateMoveList();
-	    String[] abilities = t.generateAbilities();
-	    System.out.print(t);
-	    for(int i = 0; i <moves.length; i++) {
-	        System.out.println(moves[i]);
-	    }
-	    for(int i = 0; i < abilities.length; i++) {
+
+    public static void main(String[] args) {
+        Pokemon t = new Pikachu();
+        t.setNature();
+        t.normalize();
+        t.levelUp(10);
+        String[] moves = t.generateMoveList();
+        String[] abilities = t.generateAbilities();
+        System.out.print(t);
+        for (int i = 0; i < moves.length; i++) {
+            System.out.println(moves[i]);
+        }
+        for (int i = 0; i < abilities.length; i++) {
             System.out.println(abilities[i]);
         }
-	    System.out.print(t.toJSON());
-	}
+        System.out.print(t.toJSON());
+    }
 }
